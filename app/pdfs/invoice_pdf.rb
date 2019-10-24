6e9c26014f43718301149ad326de32f5ee6eb318
Arnab Roy <arnab.roy@kreeti.com>
2019-10-24 18:18:42 +0530

    Fixed image positioning

--- app/pdfs/invoice_pdf.rb
+++ app/pdfs/invoice_pdf.rb
@@ -8,25 +8,37 @@ class InvoicePdf < Prawn::Document
   end
 
   def write_lines
-    text "Invoice", align: :center, style: :bold, size: 28
-    company_logo
+    company_details
     move_down 30
     header
     move_down 10
     body
-    bounding_box [bounds.left, bounds.bottom + 18], width: bounds.width do
-      footer
-    end
+    footer
   end
 
-  def company_logo
-    bounding_box([0, cursor], width: 100) do
-      image "#{Rails.root}/app/assets/images/logo.jpg", height: 100, width: 100 , at: [0,30]
+  def company_details
+    logo_path = "#{Rails.root}/app/assets/images/main_logo.png"
+    table([[{image: logo_path, image_height: 100, image_width: 100}, company_address]], cell_style: {border_width: 0 })
+  end
+
+  def company_address
+    data = [
+      ["Kreeti Technologies Pvt. Ltd."],
+      ["1408, Godrej Genesis"],
+      ["Website: www.kreeti.com"],
+      ["CIN: U46785385835IH6"]
+    ]
+
+    make_table(data, cell_style: {border_width: 0, width: 300, height: 21}) do
+      row(0).font_style = :bold
     end
   end
 
   def header
-    table([["", invoice_id_and_date]], column_widths: [270, 270], cell_style: {border_width: 0})
+    table([["Invoice", invoice_id_and_date]], column_widths: [270, 270], cell_style: {border_width: 0}) do
+      column(0).font_style = :bold
+      column(0).size = 30
+    end
   end
 
   def invoice_id_and_date
@@ -68,10 +80,11 @@ class InvoicePdf < Prawn::Document
   end
 
   def footer
-    table([["Total:", @invoice.order_value]], column_widths: [472.5, 67.5], cell_style: {borders: [:top]}) do
-      style(column(0), align: :right)
-      row(0).font_style = :bold
+    bounding_box [bounds.left, bounds.bottom + 18], width: bounds.width do
+      table([["Total:", @invoice.order_value]], column_widths: [472.5, 67.5], cell_style: {borders: [:top]}) do
+        style(column(0), align: :right)
+        row(0).font_style = :bold
+      end
     end
   end
-
 end
