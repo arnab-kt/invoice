1226261bd49458b8deb2a8620c1926e184794fe0
Arnab Roy <arnab.roy@kreeti.com>
2019-10-25 14:52:24 +0530

    Improved PDF View

--- app/pdfs/invoice_pdf.rb
+++ app/pdfs/invoice_pdf.rb
@@ -2,60 +2,36 @@ require 'prawn'
 class InvoicePdf < Prawn::Document
 
   def initialize(invoice)
-    super(top_margin: 70)
+    super(top_margin: 30)
     @invoice = invoice
     write_lines
   end
 
   def write_lines
-    company_details
-    move_down 30
     header
-    move_down 10
+    move_down 50
     body
     footer
   end
 
-  def company_details
-    logo_path = "#{Rails.root}/app/assets/images/main_logo.png"
-    table([[{image: logo_path, image_height: 100, image_width: 100}, company_address]], cell_style: {border_width: 0 })
-  end
-
-  def company_address
-    data = [
-      ["Kreeti Technologies Pvt. Ltd."],
-      ["1408, Godrej Genesis"],
-      ["Website: www.kreeti.com"],
-      ["CIN: U46785385835IH6"]
-    ]
-
-    make_table(data, cell_style: {border_width: 0, width: 300, height: 21}) do
-      row(0).font_style = :bold
-    end
-  end
-
-  def header
-    table([["Invoice", invoice_id_and_date]], column_widths: [270, 270], cell_style: {border_width: 0}) do
-      column(0).font_style = :bold
-      column(0).size = 30
-    end
-  end
-
-  def invoice_id_and_date
-    data = [
-      ["Invoice Number:", "##{1000000000+@invoice.id}"],
-      ["Date:", @invoice.date]
-    ]
-
-    make_table(data, column_widths: [135, 135], cell_style: {border_width: 0}) do
-      style(column(0), align: :right)
+  def footer
+    repeat :all do
+      bounding_box [bounds.left, bounds.bottom + 18], width: bounds.width do
+        table([["TOTAL ORDER VALUE:", @invoice.order_value]], column_widths: [472.5, 67.5], cell_style: {borders: [:top]}) do
+          style(column(0), align: :right)
+          row(0).font_style = :bold
+        end
+      end
     end
   end
 
   def body
-    serial_number = 1
     data = []
-    @invoice.purchase_items.each do |item|
+    serial_number = 1
+    items_per_page_counter = 1
+    page_total = 0
+    purchase_items = @invoice.purchase_items
+    purchase_items.each_with_index do |item, index|
       data <<
         [
           serial_number,
@@ -66,24 +42,121 @@ class InvoicePdf < Prawn::Document
           item.quantity,
           item.rate,
           item.amount
-        ]
+      ]
 
       serial_number +=1
+      items_per_page_counter += 1
+      page_total += item.amount
+
+      if items_per_page_counter == 2
+        items_table_header
+      end
+
+      items_table_body(data)
+      data = []
+
+      if items_per_page_counter == 11
+        items_table_footer(page_total)
+        items_per_page_counter = 1
+        page_total = 0
+        if purchase_items[index+1].present?
+          start_new_page
+          header
+          move_down 50
+        end
+      end
     end
 
-    headers = [["S.No", "Product", "Category", "Brand", "UOM", "Qty", "Rate", "Amount"]]
-    table( headers, column_widths: [37.5, 117.5, 102.5, 77.5, 42.5, 37.5, 57.5, 67.5], cell_style: {borders: [:top, :bottom]}) do
-      row(0).font_style = :bold
+    if items_per_page_counter > 1
+      items_table_footer(page_total)
     end
-
-    table( data, column_widths: [37.5, 117.5, 102.5, 77.5, 42.5, 37.5, 57.5, 67.5], cell_style: {borders: [:top]})
   end
 
-  def footer
-    bounding_box [bounds.left, bounds.bottom + 18], width: bounds.width do
-      table([["Total:", @invoice.order_value]], column_widths: [472.5, 67.5], cell_style: {borders: [:top]}) do
-        style(column(0), align: :right)
-        row(0).font_style = :bold
+  def items_table_footer(page_total)
+    table([["Total", page_total]], column_widths: [472.5, 67.5]) do
+      column(0..1).each do |c|
+        c.align = :right
+      end
+      row(0).font_style = :bold
+    end
+  end
+
+  def items_table_header
+    headers = [["S.No", "Product", "Category", "Brand", "UOM", "Qty", "Rate", "Amount"]]
+    table( headers, column_widths: [37.5, 117.5, 102.5, 77.5, 42.5, 37.5, 57.5, 67.5]) do
+      row(0).font_style = :bold
+      column(0).align = :right
+      column(5..7).each do |c|
+        c.align = :right
+      end
+    end
+  end
+
+
+  def items_table_body(data)
+    table( data, column_widths: [37.5, 117.5, 102.5, 77.5, 42.5, 37.5, 57.5, 67.5], cell_style: { padding_top: 16, padding_bottom: 16}) do
+      column(0).borders = [:left]
+      column(0).align = :right
+      column(1..6).each do |c|
+        c.borders = [:left, :right]
+      end
+      column(7).borders = [:right]
+
+      column(5..7).each do |c|
+        c.align = :right
+      end
+    end
+  end
+
+  def header
+    repeat :all do
+      bounding_box [bounds.left, bounds.top], width: bounds.width do
+        logo_path = "#{Rails.root}/app/assets/images/main_logo.png"
+        table([
+          [ {image: logo_path, image_height: 103, image_width: 100}, company_name, invoice_id_and_date]
+        ],cell_style: {border_width: 0, padding: 0}, column_widths: [100, 180, 260]) do
+          style(column(2), align: :right)
+          column(2).font_style = :bold
+          column(2).size = 30
+        end
+      end
+    end
+  end
+
+  def invoice_id_and_date
+    data = [
+      [{content: "INVOICE", colspan: 2}],
+      ["Invoice Number:", "##{1000000000+@invoice.id}"],
+      ["Date:", @invoice.date]
+    ]
+
+    make_table(data, cell_style: {border_width: 0}, column_widths: [175, 85]) do
+      style(column(0), align: :right)
+      row(0).column(0).font_style = :bold
+      row(0).column(0).size =28
+    end
+  end
+
+
+  def company_name
+    data = [
+      ["Kreeti Technologies Pvt. Ltd."],
+      ["Innovation And Development"],
+      [""],
+      ["1408, Godrej Genesis"],
+      ["Website: www.kreeti.com"],
+      ["CIN: U46785385835IH6"]
+    ]
+
+    make_table(data, cell_style: {border_width: 0, width: 300, height: 21}, column_widths: [180]) do
+      row(0).font_style = :bold
+      rows(0..1).each do |r|
+        r.size = 10
+      end
+
+      rows(2..5).each do |r|
+        r.height = 16
+        r.size = 7
       end
     end
   end
