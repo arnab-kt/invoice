ca71e790897e92e5622b4e048550e898bd6dc16f
Arnab Roy <arnab.roy@kreeti.com>
2019-10-29 17:58:22 +0530

    changed styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,11 +9,12 @@ module ApplicationHelper
     link_to(name, "#", class: "add_fields", data: {id: id, fields: fields.gsub("/n","")})
   end
 
-  def delete_link(path)
+  def delete_link(path, class_name="")
     link_to ('<i class="fa fa-trash " title="Delete Details"></i>').html_safe,
               path,
               method: :delete,
-              data: { confirm: "Are you sure?" }
+              data: { confirm: "Are you sure?" },
+              class: "#{class_name}"
   end
 
 end
